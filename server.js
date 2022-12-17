fc992776e5c10ecf515ff9d3c78c61e7ba1abb73
ekow <ekowfirmino@gmail.com>
2022-12-17 14:45:47 +0000

    fixed bug

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const bodyParser = require('body-parser')
 const server = express()
 
 // using this manual method means ,you have to explicitly write handler for the other files
-const home = (req ,res) =>{
-   console.log(req.body)
-   res.end("<h1>Congratulation.........</h1>")
-}
+
+  const loginHandler = (req , res) => {
+   
+    console.log(req.body)
+   res.send('Done')
+  }
 
 
 
-
+ 
 server.use(express.static(path.join(__dirname , 'public')))
 server.use(bodyParser.urlencoded({extended : false}))
 
-server.post('/login' , home)
+server.post("/login" , loginHandler)
+
 
 server.listen(3000 , () => console.log('server is listening on 3000'))
